4c25027b8b9703c120eb050f5cc61ad5056a3ca3
ginuerzh <ginuerzh@gmail.com>
2023-10-31 22:57:54 +0800

    add sd

--- go.mod
+++ go.mod
@@ -1,16 +1,34 @@
 module github.com/go-gost/plugin
 
-go 1.20
+go 1.21
+
+toolchain go1.21.2
 
 require (
-	google.golang.org/grpc v1.58.1
+	github.com/go-gost/x v0.0.0-20231029022713-e8d5e719a434
+	google.golang.org/grpc v1.59.0
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.14.0 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231016165738-49dd2c1f3d0b // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ sd/example/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/go-gost/plugin/sd/proto"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 8000, "The server port")
+)
+
+type server struct {
+	proto.UnimplementedSDServer
+}
+
+func (s *server) Register(ctx context.Context, in *proto.RegisterRequest) (*proto.RegisterReply, error) {
+	reply := &proto.RegisterReply{}
+	log.Printf("register: %s, %s, %s", in.GetName(), in.GetNetwork(), in.GetAddress())
+	return reply, nil
+}
+
+func (s *server) Deregister(ctx context.Context, in *proto.DeregisterRequest) (*proto.DeregisterReply, error) {
+	reply := &proto.DeregisterReply{}
+	log.Printf("deregister: %s", in.GetName())
+	return reply, nil
+}
+
+func (s *server) Renew(ctx context.Context, in *proto.RenewRequest) (*proto.RenewReply, error) {
+	reply := &proto.RenewReply{}
+	log.Printf("renew: %s", in.GetName())
+	return reply, nil
+}
+
+func (s *server) Get(ctx context.Context, in *proto.GetServiceRequest) (*proto.GetServiceReply, error) {
+	log.Printf("get: %s", in.GetName())
+	reply := &proto.GetServiceReply{
+		Services: []*proto.Service{
+			{
+				Name:    "c25cab37-4d84-4c34-8c30-4916a26b8fe9",
+				Network: "tcp",
+				Address: "127.0.0.1:8420",
+			},
+			{
+				Name:    "b83294f5-34b6-4698-b15d-b3c11b81ec1c",
+				Network: "udp",
+				Address: "127.0.0.1:8420",
+			},
+		},
+	}
+	return reply, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	proto.RegisterSDServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
